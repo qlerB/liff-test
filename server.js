35ef41d9998ffa614ee9ddcde745e4fa5f173859
John <tanjohn430@gmail.com>
2026-08-24 17:36:45 +0700

    prepare backend for vercel

--- server.js
+++ server.js
@@ -14,8 +14,4 @@ app.post("/api/login", (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
